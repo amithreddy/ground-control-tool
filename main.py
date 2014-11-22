06b80357c4469cc5a84ccf2397296e8c7d9dcf3f
Amith Reddy <amithmreddy@gmail.com>
2014-11-22 15:03:35 -0500

    removed some fluff

--- main.py
+++ main.py
@@ -97,32 +97,17 @@ class ApplicationWindow(QtGui.QMainWindow):
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.setWindowTitle("application main window")
 
-        self.file_menu = QtGui.QMenu('&File', self)
-        self.file_menu.addAction('&Quit', self.fileQuit,
-                                 QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
-        self.menuBar().addMenu(self.file_menu)
-
-        self.help_menu = QtGui.QMenu('&Help', self)
-        self.menuBar().addSeparator()
-        self.menuBar().addMenu(self.help_menu)
-
-        self.help_menu.addAction('&About', self.about)
-
         self.main_widget = QtGui.QWidget(self)
 
+        """
         l = QtGui.QVBoxLayout(self.main_widget)
         sc = MyStaticMplCanvas(self.main_widget, points=points,width=5, height=4, dpi=100)
         l.addWidget(sc)
 
         self.main_widget.setFocus()
         self.setCentralWidget(self.main_widget)
+        """
 
-    def fileQuit(self):
-        self.close()
-    def closeEvent(self, ce):
-        self.fileQuit()
-    def about(self):
-        QtGui.QMessageBox.about(self, "About","""arst.""")
 
 points = [(0,0,0),(1,1,0),(2,1,0),(1,0,0),
         (0,0,1),(1,1,1),(2,1,1),(1,0,1)
