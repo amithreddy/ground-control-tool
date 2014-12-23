1d2e4e4363c76e4120b55cef5af53903e4439336
Amith Reddy <amithmreddy@gmail.com>
2014-12-22 19:15:51 -0500

    added 3d projection

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from PyQt4 import QtGui, QtCore
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.path as mpath
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 import mining_ui
 from geometry import *
@@ -15,34 +17,32 @@ from geometry import *
 progname = os.path.basename(sys.argv[0])
 progversion = "0.1"
 
-class MyMplCanvas(FigureCanvas):
+class MplCanvas(FigureCanvas):
     """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
         # We want the axes cleared every time plot() is called
         self.axes.hold(False)
-
-
+        self.figure= FigureCanvas
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
 
         FigureCanvas.setSizePolicy(self,QtGui.QSizePolicy.Minimum,
                                    QtGui.QSizePolicy.Minimum)
         FigureCanvas.updateGeometry(self)
-
     def sizeHint(self):
         w, h =self.get_width_height()
         return QtCore.QSize(w,h)
 
-class MyStaticMplCanvas(MyMplCanvas):
+class _2DMplCanvas(MplCanvas):
     """Simple canvas with a sine plot."""
     def __init__(self,*args,**kwargs):
         self.points=kwargs["points"]
         self.view=kwargs["view"]
         del kwargs["points"]
         del kwargs["view"]
-        MyMplCanvas.__init__(self,*args,**kwargs)
+        MplCanvas.__init__(self,*args,**kwargs)
 
         self.compute_initial_figure(self.view)
     def draw_view(self,points,view=None):
@@ -68,7 +68,7 @@ class MyStaticMplCanvas(MyMplCanvas):
         max_ =max(itertools.chain(set1,set2))
         x,y= self.line(set1)
         self.axes.plot(x,y,color1+'s-',linewidth=2.0)
-
+        
         x,y= self.line(set2)
         self.axes.plot(x,y,color2+'s-',linewidth=2.0)
         
@@ -99,6 +99,41 @@ class MyStaticMplCanvas(MyMplCanvas):
     def compute_initial_figure(self,view):
         self.draw_view(self.points,view=view)
 
+class _3DMplCanvas(FigureCanvas):
+    def __init__(self,parent=None,width=5,height=4,dpi=100,points=None):
+        self.points=points
+        self.fig = plt.figure(figsize=(width,height),dpi=dpi)
+        self.axes = self.fig.add_subplot(111, projection='3d') 
+        self.axes.hold(True)
+        self.figure= FigureCanvas
+        FigureCanvas.__init__(self, self.fig)
+        self.setParent(parent)
+        FigureCanvas.setSizePolicy(self,QtGui.QSizePolicy.Minimum,
+                                   QtGui.QSizePolicy.Minimum)
+        FigureCanvas.updateGeometry(self)
+
+        self.compute_initial_figure()
+    def sizeHint(self):
+        w, h =self.get_width_height()
+        return QtCore.QSize(w,h)
+    def unpack(self,points):
+        """ takes[(x,y,z),(1,1,1),(2,2,2)] and returns
+            [x,1,2],[y,1,2],[z,1,2] """
+        x,y,z= ([point[0] for point in points],
+                [point[1] for point in points],
+                [point[2] for point in points]
+                )
+        return x,y,z
+        return self.fig.add_subplot(111, projection='3d')
+    def draw_plot(self):
+        p1,p2,p3,p4,p5,p6,p7,p8 = self.points
+        self.axes.plot3D(*self.unpack([p1,p2,p3,p4,p1,p5,p6,p7,p8,p5]))
+        self.axes.plot3D(*self.unpack([p4,p8]), color="green")
+        self.axes.plot3D(*self.unpack([p2,p6]), color="blue")
+        self.axes.plot3D(*self.unpack([p7,p3]), color="blue")
+    def compute_initial_figure(self):
+        self.draw_plot()
+
 class ApplicationWindow(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
@@ -110,11 +145,13 @@ class ApplicationWindow(QtGui.QMainWindow):
         grid= QtGui.QGridLayout()
         grid.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
 
-        for index, val in enumerate(["plan","front","side"]):
-            l =MyStaticMplCanvas(self.main_widget,view=val, points=points,
-                    width=3, height=2, dpi=100)
-            grid.addWidget(l,0,index)
-
+        #for index, val in enumerate(["plan","front","side"]):
+        #    l = _2DMplCanvas(self.main_widget,view=val, points=points,
+        #            width=3, height=2, dpi=100)
+        #    grid.addWidget(l,0,index)
+        l = _3DMplCanvas(self.main_widget,points=points,width=3,
+                        height=2,dpi=100)
+        grid.addWidget(l,0,4)
         horizontal= self.ui.horizontalLayout
         horizontal.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
         horizontal.addLayout(grid)
