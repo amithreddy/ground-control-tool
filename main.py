92d6fc2c22a6a30b9452532647f0e237ddbd3c1b
Amith Reddy <amithmreddy@gmail.com>
2015-01-03 15:48:31 -0500

    refractored to remove two different classes into one

--- main.py
+++ main.py
@@ -37,34 +37,32 @@ color ={
         'left': 'black', 'right':'green'
         }
 
-class MplCanvas(FigureCanvas):
-    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        self.fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = self.fig.add_subplot(111)
-        # We want the axes cleared every time plot() is called
-        self.axes.hold(True)
-        FigureCanvas.__init__(self, self.fig)
-        self.setParent(parent)
 
+class _3DMplCanvas(FigureCanvas):
+    def __init__(self,parent=None,width=5,height=4,dpi=100,points=None):
+        self.points=points
+        self.fig = plt.figure(figsize=(width,height),dpi=dpi)
+        FigureCanvas.__init__(self, self.fig)
         FigureCanvas.setSizePolicy(self,QtGui.QSizePolicy.Minimum,
                                    QtGui.QSizePolicy.Minimum)
         FigureCanvas.updateGeometry(self)
-    def sizeHint(self):
-        w, h =self.get_width_height()
-        return QtCore.QSize(w,h)
 
-class _2DMplCanvas(MplCanvas):
-    """Simple canvas with a sine plot."""
-    def __init__(self,*args,**kwargs):
-        self.points=kwargs["points"]
-        self.view=kwargs["view"]
-        del kwargs["points"]
-        del kwargs["view"]
-        MplCanvas.__init__(self,*args,**kwargs)
-
-        self.compute_initial_figure(self.view)
-    def draw_view(self,points,view=None):
+        self.front = self.create_subplot(221) 
+        self.plan = self.create_subplot(222) 
+        self.side = self.create_subplot(223)
+        self.axes3d = self.create_subplot(224, _3d='True') 
+        self.setParent(parent)
+        self.compute_initial_figure()
+    def create_subplot(self,number,_3d=False):
+        if _3d:
+            axes= self.fig.add_subplot(number,projection='3d')
+            axes.mouse_init()
+        else:
+            axes = self.fig.add_subplot(number)        
+        axes.hold(True)
+        return axes
+    def draw_view(self,view=None):
+        points= self.points
         x,y,z=0,1,2
         if view=="plan":i,c=x,y
         elif view=="front":i,c=x,z
@@ -79,47 +77,63 @@ class _2DMplCanvas(MplCanvas):
             'left': [p1,p5,p6,p2],
             'right':[p7,p3, p8,p4]
             }
-        def front_or_side():
-            self.draw_plot(view, views['top'], color=color['top'])
-            self.draw_plot(view, unpack(views['left'][:2]), color=color['left'])
-            self.draw_plot(view, unpack(views['left'][2:]), color=color['left'])
-            self.draw_plot(view, unpack(views['right'][:2]), color=color['right'])
-            self.draw_plot(view, unpack(views['right'][2:]), color=color['right'])
-            self.draw_plot(view, views['bottom'], color=color['bottom'])
+        def front_or_side(axes):
+            self.draw_plot(view, views['top'],axes, color=color['top'])
+            self.draw_plot(view, unpack(views['left'][:2]), axes, color=color['left'])
+            self.draw_plot(view, unpack(views['left'][2:]), axes, color=color['left'])
+            self.draw_plot(view, unpack(views['right'][:2]), axes, color=color['right'])
+            self.draw_plot(view, unpack(views['right'][2:]), axes, color=color['right'])
+            self.draw_plot(view, views['bottom'], axes, color=color['bottom'])
         if view=="plan":
-            self.draw_plot(view, views['bottom'], color=color['bottom'])
-            self.draw_plot(view, views['top'], color=color['top'])
+            self.draw_plot(view, views['bottom'], self.plan, color=color['bottom'])
+            self.draw_plot(view, views['top'], self.plan, color=color['top'])
+            self.adjust_lim(self.plan)
         elif view=="front":
-            front_or_side()
+            front_or_side(self.front)
+            self.adjust_lim(self.front)
         elif view=="side":
-            front_or_side()
-        self.adjust_lim()
-    def draw_plot(self,view,_set, color='black'):
-        self.fig.suptitle(view,fontsize=12)
+            front_or_side(self.side)
+            self.adjust_lim(self.side)
+    def draw_plot3d(self,axes):
+        p1,p2,p3,p4,p5,p6,p7,p8 = self.points
+        views = {
+                'top':[p5,p6,p7,p8,p5],
+                'bottom':[p1,p2,p3,p4,p1],
+                'left': [p1,p5,p6,p2],
+                'right':[p7,p3,p8,p4]
+                }
+        axes.plot3D(*unpack(views['top']), color=color['top'])
+        axes.plot3D(*unpack(views['left'][:2]), color=color['left'])
+        axes.plot3D(*unpack(views['left'][2:]), color=color['left'])
+        axes.plot3D(*unpack(views['right'][2:]), color=color['right'])
+        axes.plot3D(*unpack(views['right'][:2]), color=color['right'])
+        axes.plot3D(*unpack(views['bottom']), color=color['bottom'])
+    def draw_plot(self,view,_set,axes,color='black'):
+        axes.set_title(view,fontsize=12)
         if len(_set)==4:
             x,y= self.line(_set)
         else:
             x,y= _set
-        self.axes.set_autoscale_on(True)
-        self.axes.grid()
-        self.axes.axis('equal')
-        self.axes.plot(x,y,color=color,linewidth=2.0)
-    def adjust_lim(self):        
-        xticks= self.axes.get_xticks()
+        axes.set_autoscale_on(True)
+        axes.grid()
+        axes.axis('equal')
+        axes.plot(x,y,color=color,linewidth=2.0)
+    def adjust_lim(self,axes):        
+        xticks= axes.get_xticks()
         #shift a half a step to the left
         # x0 - (x1- x0)/ 2 = (3*x0-x1)/2
         xmin = (3*xticks[0] - xticks[1])/2
         #shift a half a tick to the right
         xmax = (3*xticks[-1] - xticks[-2])/2
-        self.axes.set_xlim(xmin,xmax)
+        axes.set_xlim(xmin,xmax)
 
-        yticks= self.axes.get_yticks()
+        yticks= axes.get_yticks()
         #shift a half a step to the left
         # y0 - (y1- y0)/ 2 = (3*y0-y1)/2
         ymin = (3*yticks[0] - yticks[1])/2
         #shift a half a tick to the right
         ymax = (3*yticks[-1] - yticks[-2])/2
-        self.axes.set_ylim(ymin,ymax)
+        axes.set_ylim(ymin,ymax)
     def line(self,points):
         Path = mpath.Path
         p1,p2,p3,p4=points
@@ -136,41 +150,14 @@ class _2DMplCanvas(MplCanvas):
         # plot control points and connecting lines
         x, y = zip(*path.vertices)
         return x,y
-    def compute_initial_figure(self,view):
-        self.draw_view(self.points,view=view)
-
-class _3DMplCanvas(FigureCanvas):
-    def __init__(self,parent=None,width=5,height=4,dpi=100,points=None):
-        self.points=points
-        self.fig = plt.figure(figsize=(width,height),dpi=dpi)
-        self.axes = self.fig.add_subplot(111, projection='3d') 
-        self.axes.hold(True)
-        FigureCanvas.__init__(self, self.fig)
-        self.axes.mouse_init()
-        self.setParent(parent)
-        FigureCanvas.setSizePolicy(self,QtGui.QSizePolicy.Minimum,
-                                   QtGui.QSizePolicy.Minimum)
-        FigureCanvas.updateGeometry(self)
-        self.compute_initial_figure()
+    def compute_initial_figure(self):
+        self.draw_view(view='front')
+        self.draw_view(view='side')
+        self.draw_view(view='plan')
+        self.draw_plot3d(self.axes3d)
     def sizeHint(self):
         w, h =self.get_width_height()
         return QtCore.QSize(w,h)
-    def draw_plot(self):
-        p1,p2,p3,p4,p5,p6,p7,p8 = self.points
-        views = {
-                'top':[p5,p6,p7,p8,p5],
-                'bottom':[p1,p2,p3,p4,p1],
-                'left': [p1,p5,p6,p2],
-                'right':[p7,p3, p8,p4]
-                }
-        self.axes.plot3D(*unpack(views['top']), color=color['top'])
-        self.axes.plot3D(*unpack(views['left'][:2]), color=color['left'])
-        self.axes.plot3D(*unpack(views['left'][2:]), color=color['left'])
-        self.axes.plot3D(*unpack(views['right'][2:]), color=color['right'])
-        self.axes.plot3D(*unpack(views['right'][:2]), color=color['right'])
-        self.axes.plot3D(*unpack(views['bottom']), color=color['bottom'])
-    def compute_initial_figure(self):
-        self.draw_plot()
 
 class StaticGraph(FigureCanvas):
     """ Fixed y and x axis. On running plots a line, and updates it with
@@ -189,19 +176,9 @@ class ApplicationWindow(QtGui.QMainWindow):
         grid= QtGui.QGridLayout()
         grid.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
        
-        QtCore
-        print dir(self.ui.submit)
-        row,column =0,0
-        for index, val in enumerate(["plan","front","side"]):
-            l = _2DMplCanvas(self.main_widget,view=val, points=points,
-                    width=3, height=2, dpi=100)
-            grid.addWidget(l,row,column)
-            column+=1
-            if column==2:
-                column=0; row= 1
         l = _3DMplCanvas(self.main_widget,points=points,width=3,
                         height=2,dpi=100)
-        grid.addWidget(l,1,1)
+        grid.addWidget(l,0,0)
         horizontal= self.ui.horizontalLayout
         horizontal.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
         horizontal.addLayout(grid)
