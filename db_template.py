011d2d5449445ca8664dfcb1db6a9bb69c1fc984
Amith Reddy <amithmreddy@gmail.com>
2015-02-28 16:00:00 -0500

    Refactored database generator

--- db_template.py
+++ db_template.py
@@ -1,27 +1,47 @@
 from sqlqueries import *
 from main import sqldb
 import shutil,os
-name = 'generateddb'
-try:
-    os.remove(name)
-except OSError:
-    pass
-db = sqldb(name)
 
 #populate the headertable 
-header_keys= ['mine', 'orebody','level','stopename']
-for x in range(0,20):
+def insert_header(db,id):
+    values ={key:key+str(id) for key in header_keys}
     db.query_db(insert_header,
-        bindings={key:key+str(x) for key in header_keys
-                        })
-shape_keys= ['b1','b2','b3','b4','t1','t2','t3','t4']
-for x in range(0,20):
+        bindings=values)
+    return values
+header_keys= ['mine', 'orebody','level','stopename']
+def populate_header(db):
+    for x in range(0,20):
+        insert_header(db,x)
+
+#populate the shapetable
+def insert_shape(db,id):
+    shape_keys= ['b1','b2','b3','b4','t1','t2','t3','t4']
     values= {key:key+str(x) for key in shape_keys}
     values['id']=str(x)
     db.query_db(shape_insert,
             bindings=values)
-try:
-    shutil.move('generateddb','tests/')
-except shutil.Error:
-    os.remove('tests/generateddb')
-    shutil.move('generateddb','tests/')
+    return values
+def select_shape(x):
+    shape_keys= ['b1','b2','b3','b4','t1','t2','t3','t4']
+    print 'select shape', {key:key+str(x) for key in shape_keys}
+    return {key:key+str(x) for key in shape_keys}
+def populate_shape(db):
+    for x in range(0,20):
+        insert_shape(db,x)    
+
+def main():
+    name = 'generateddb'
+    try:
+        os.remove(name)
+    except OSError:
+        pass
+    db = sqldb(name)
+    populate_header(db)
+    populate_shape(db)
+    try:
+        shutil.move('generateddb','tests/')
+    except shutil.Error:
+        os.remove('tests/generateddb')
+        shutil.move('generateddb','tests/')
+if __name__ =="__main__":
+    main()
