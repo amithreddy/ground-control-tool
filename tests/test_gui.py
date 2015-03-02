a9c071eb9348fc39a9ebdefaacccdd0a73144f25
Amith Reddy <amithmreddy@gmail.com>
2015-03-02 13:36:34 -0500

    fixed bug for gui test, the tab had to be visible in order for the test to work

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -7,6 +7,7 @@ from PyQt4 import QtGui, QtCore
 import sys, os
 import main
 
+# run this every time you run pyuic4 ( make changes to your ui)
 qApp=None
 main.mkQApp()
 @attr('gui')
@@ -17,19 +18,19 @@ class TabsTest(unittest.TestCase):
         cls.db =main.sqldb(name=cls.name)
         cls.form = main.ApplicationWindow(cls.db)
     def test_shapefieldorder(self):
-        # refactor into a seperator testcase for all tabs? this way you only hav
-        # to run this every time you run pyuic4 ( make changes to your ui)
+        # ensure the visual order of the fields is maintained
         self.geometry_fields = [ #ordered by apperance visually, top to bottom
                 self.form.ui.t1,self.form.ui.t2,self.form.ui.t3,self.form.ui.t4,
                 self.form.ui.b1,self.form.ui.b2,self.form.ui.b3,self.form.ui.b4
                     ]
         # render the form, neccessary for this test otherwise all coords return 0
         self.form.show()
-        # ensure the visual order of the fields is maintained
+        self.form.ui.tabWidget.setCurrentWidget(self.form.ui.Shape)
         points = [field.y() for field in self.geometry_fields]
         assert(sum(points)!=0)
         self.assertListEqual(points,sorted(points))
     @classmethod
     def tearDownClass(cls):
+        cls.form.close()
         cls.db.close()
         os.remove(cls.name)
